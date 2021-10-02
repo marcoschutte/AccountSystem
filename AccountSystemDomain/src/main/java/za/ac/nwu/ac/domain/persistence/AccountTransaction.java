948d880d3cc8f33d94ae58faa6c852a716a8322f
Marco Schutte <marcoschutte1234@gmail.com>
2021-10-02 12:16:21 +0200

    commit AccountType.java

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountTransaction.java
@@ -1,5 +1,22 @@
 package za.ac.nwu.ac.domain.persistence;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 
 public class AccountTransaction {
+
+    }
 }
--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/AccountType.java
@@ -3,8 +3,91 @@ package za.ac.nwu.ac.domain.persistence;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Table
-public class AccountType  {
+@Table(name = "DEMO_ACCOUNT_TYPE", schema = "VITRSA_SANDBOX")
+public class AccountType implements Serializable {
+
+    private static final long serialVersionUID = 3833725316797154577L;
+
+    @Id
+    @SequenceGenerator(name = "VIT_RSA_GENERIC_SEQ", sequenceName = "VITRSA_SANDBOX.VIT_RSA_GENERIC_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VIT_RSA_GENERIC_SEQ")
+    @Column(name = "ACCOUNT_TYPE_ID")
+    private Long accountTypeId;
+
+    @Column(name = "MNEMONIC")
+    private String mnemonic;
+
+    @Column(name = "ACCOUNT_TYPE_NAME")
+    private String accountTypeName;
+
+    @Column(name = "CREATION_DATE")
+    private LocalDate creationDate;
+
+    public AccountType(Long accountTypeId, String mnemonic, String accountTypeName, LocalDate creationDate) {
+        this.accountTypeId = accountTypeId;
+        this.mnemonic = mnemonic;
+        this.accountTypeName = accountTypeName;
+        this.creationDate = creationDate;
+    }
+
+    public AccountType() {
+    }
+
+    public Long getAccountTypeId() {
+        return accountTypeId;
+    }
+
+    public void setAccountTypeId(Long accountTypeId) {
+        this.accountTypeId = accountTypeId;
+    }
+
+    public String getMnemonic() {
+        return mnemonic;
+    }
+
+    public void setMnemonic(String mnemonic) {
+        this.mnemonic = mnemonic;
+    }
+
+    public String getAccountTypeName() {
+        return accountTypeName;
+    }
+
+    public void setAccountTypeName(String accountTypeName) {
+        this.accountTypeName = accountTypeName;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountType that = (AccountType) o;
+        return Objects.equals(accountTypeId, that.accountTypeId) && Objects.equals(mnemonic, that.mnemonic) && Objects.equals(accountTypeName, that.accountTypeName) && Objects.equals(creationDate, that.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountTypeId, mnemonic, accountTypeName, creationDate);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountType{" +
+                "accountTypeId=" + accountTypeId +
+                ", mnemonic='" + mnemonic + '\'' +
+                ", accountTypeName='" + accountTypeName + '\'' +
+                ", creationDate=" + creationDate +
+                '}';
+    }
 }
