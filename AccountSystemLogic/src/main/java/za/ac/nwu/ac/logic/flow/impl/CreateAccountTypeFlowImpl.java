01c8850fb771c8995d331a50738f12439b0f3ae9
Marco Schutte <marcoschutte1234@gmail.com>
2021-10-04 22:06:59 +0200

    commit CreateAccountTypeFlowImpl.java

--- AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeFlowImpl.java
+++ AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/impl/CreateAccountTypeFlowImpl.java
@@ -1,13 +1,12 @@
 package za.ac.nwu.ac.logic.flow.impl;
 
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.logic.flow.CreateAccountTypeFlow;
 import za.ac.nwu.ac.translator.AccountTypeTranslator;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
-
 
 @Transactional
 @Component("createAccountTypeFlowName")
@@ -15,16 +14,14 @@ public class CreateAccountTypeFlowImpl implements CreateAccountTypeFlow
 {
     private final AccountTypeTranslator accountTypeTranslator;
 
-    public CreateAccountTypeFlowImpl(AccountTypeTranslator accountTypeTranslator)
-    {
+    public CreateAccountTypeFlowImpl(AccountTypeTranslator accountTypeTranslator) {
         this.accountTypeTranslator = accountTypeTranslator;
     }
 
     @Override
     public AccountTypeDto create(AccountTypeDto accountType)
     {
-        if(null== accountType.getCreationDate())
-        {
+        if(null== accountType.getCreationDate()) {
             accountType.setCreationDate(LocalDate.now());
         }
         return accountTypeTranslator.create(accountType);
