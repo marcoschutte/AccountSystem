09d70e5ca5d379337180d6e241ddbbfd2fdf5c68
Marco Schutte <marcoschutte1234@gmail.com>
2021-10-08 21:54:50 +0200

    commit AccountTransactionTranslatorImpl.java

---
+++ AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/impl/AccountTransactionTranslatorImpl.java
@@ -0,0 +1,58 @@
+package za.ac.nwu.ac.translator.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import za.ac.nwu.ac.domain.dto.AccountTransactionDto;
+import za.ac.nwu.ac.domain.dto.AccountTypeDto;
+import za.ac.nwu.ac.domain.persistence.AccountTransaction;
+import za.ac.nwu.ac.domain.persistence.AccountType;
+import za.ac.nwu.ac.repo.persistence.AccountTransactionRepository;
+import za.ac.nwu.ac.translator.AccountTransactionTranslator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AccountTransactionTranslatorImpl implements AccountTransactionTranslator
+{
+    private final AccountTransactionRepository accountTransactionRepository;
+
+    @Autowired
+    public AccountTransactionTranslatorImpl(AccountTransactionRepository accountTransactionRepository)
+    {
+        this.accountTransactionRepository = accountTransactionRepository;
+    }
+
+    @Override
+    public List<AccountTransactionDto> getAllAccountTransactions()
+    {
+        List<AccountTransactionDto> accountTransactionDtos = new ArrayList<>();
+
+        try
+        {
+            for (AccountTransaction accountTransaction : accountTransactionRepository.findAll())
+            {
+                accountTransactionDtos.add(new AccountTransactionDto(accountTransaction));
+            }
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Unable to read from the DB",e);
+        }
+        return accountTransactionDtos;
+    }
+
+    @Override
+    public AccountTransactionDto create(AccountTransactionDto accountTransactionDto)
+    {
+        try
+        {
+            AccountTransaction accountTransaction = accountTransactionRepository.save(accountTransactionDto.getAccountTransaction());
+            return new AccountTransactionDto(accountTransaction);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Unable to save to the DB",e);
+        }
+    }
+}
