1cabafb98e3426977422dc93cedb0183dcea0399
Marco Schutte <marcoschutte1234@gmail.com>
2021-10-08 21:54:39 +0200

    commit AccountTransactionTranslator.java

---
+++ AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTransactionTranslator.java
@@ -0,0 +1,12 @@
+package za.ac.nwu.ac.translator;
+
+import za.ac.nwu.ac.domain.dto.AccountTransactionDto;
+
+import java.util.List;
+
+public interface AccountTransactionTranslator
+{
+    List<AccountTransactionDto> getAllAccountTransactions();
+
+    AccountTransactionDto create(AccountTransactionDto accountTransaction);
+}
