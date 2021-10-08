453718fbee0d689a917b4324807718358854c3b1
Marco Schutte <marcoschutte1234@gmail.com>
2021-10-08 17:41:05 +0200

    commit AccountTypeTranslator.java

--- AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTypeTranslator.java
+++ AccountSystemTranslator/src/main/java/za/ac/nwu/ac/translator/AccountTypeTranslator.java
@@ -12,8 +12,8 @@ public interface AccountTypeTranslator {
 
     AccountTypeDto getAccountTypeByMnemonicNativeQuery(String mnemonic);
 
-    AccountTypeDto getAccountTypeByMnemonic(String mnemonic);
+    //AccountTypeDto getAccountTypeByMnemonic(String mnemonic);
 
-    AccountTypeDto getAccountTypeDtoByMnemonic(String mnemonic);
+    //AccountTypeDto getAccountTypeDtoByMnemonic(String mnemonic);
 
 }
