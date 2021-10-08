1e2d881f47e70b95faeceb11c2271e3400e57e56
Marco Schutte <marcoschutte1234@gmail.com>
2021-10-08 21:55:39 +0200

    commit AccountTypeRepository.java

--- AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
+++ AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/persistence/AccountTypeRepository.java
@@ -5,33 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import za.ac.nwu.ac.domain.dto.AccountTypeDto;
 import za.ac.nwu.ac.domain.persistence.AccountType;
 
-public interface AccountTypeRepository extends JpaRepository<AccountType, Long> {
+public interface AccountTypeRepository extends JpaRepository<AccountType, Long>
+{
 
-    @Query(value = "SELECT " +
-            "       ACCOUNT_TYPE_ID," +
-            "       MNEMONIC," +
-            "       ACCOUNT_TYPE_NAME," +
-            "       CREATION_DATE" +
-            "   FROM " +
-            "       HR.ACCOUNT_TYPE " +
-            "   WHERE MNEMONIC = :mnemonic ", nativeQuery = true)
-    AccountType getAccountTypeByMnemonicNativeQuery(String mnemonic);
-/*
-    @Query(value = "SELECT " +
-            "       at" +
-            "   FROM " +
-            "       AccountType at" +
-            "   WHERE at.mnemonic = :mnemonic ")
-    AccountType getAccountTypeByMnemonic(String mnemonic);
-
-    @Query(value = "SELECT new za.ac.nwu.ac.domain.dto.AccountTypeDto( " +
-            "       at.mnemonic," +
-            "       at.accountTypeName," +
-            "       at.creation )" +
-            "   FROM " +
-            "       AccountType at" +
-            "   WHERE at.mnemonic = :mnemonic ")
-    AccountTypeDto getAccountTypeDtoByMnemonic(String mnemonic);
-
-*/
 }
